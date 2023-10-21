c01ba8cad3c09ac50fa4075100675b79ee2a149b
MihailPaduraru <mch.mihaii@yahoo.com>
2023-10-21 23:28:50 +0100

    Homework done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
+const ingredientsList = document.getElementById("ingredients");
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+ingredients.forEach((ingredient) => {
+  const liElement = document.createElement("li");
+
+  liElement.textContent = ingredient;
+
+  liElement.classList.add("item");
+
+  ingredientsList.appendChild(liElement);
+});
